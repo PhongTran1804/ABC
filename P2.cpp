4b1a2b59d5dd8d2d8777966370133b12ba03e2d2
PhongTran1804 <74850769+PhongTran1804@users.noreply.github.com>
2020-11-22 13:46:40 +0700

    Add files via upload

---
+++ P2.cpp
@@ -0,0 +1,8 @@
+#include "P2.h"
+int main()
+{
+	int a,b;
+	cout << "Nhap so nguyen duong a va b: "; cin >> a>>b;
+	cout << "Uoc chung lon nhat cua " << a << " va " << b << " la: " << ucln(a, b)<<endl;
+	cout << "Boi chung nho nhat cua " << a << " va " << b << " la: " << bcnn(a, b)<<endl;
+}
